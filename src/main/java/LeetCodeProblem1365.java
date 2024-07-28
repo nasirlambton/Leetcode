01e4e04c81a17bdd89b586c6f4b4594ad4efd013
nasirlambton <nasirlambton@gmail.com>
2024-07-28 14:06:51 -0400

    added solution class

--- src/main/java/LeetCodeProblem1365.java
+++ src/main/java/LeetCodeProblem1365.java
@@ -3,7 +3,29 @@ public class LeetCodeProblem1365 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
+	}
+	
+	
+	class Solution {
+	    public int[] smallerNumbersThanCurrent(int[] nums) {
+	        int count[] = new int [nums.length];
+	        int counter=0;
+	        for(int i=0;i<nums.length;i++){
+	            for(int j=i+1;j<nums.length;j++){
+	                
+	                if(nums[i]>nums[j]){
+	                    counter++;
+	                    System.out.print(counter);                    
+	                    count[i] = counter; 
+	                    System.out.print(count[i]);
+	                }
+	            }
+
+	        }
+	        return count;
+	    }
 	}
 
 }
