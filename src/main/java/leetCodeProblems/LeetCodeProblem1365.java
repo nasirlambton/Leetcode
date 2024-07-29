16919d9150ae265fec6b3739718a8064b7f939b0
nasirlambton <nasirlambton@gmail.com>
2024-07-28 21:40:05 -0400

    1365

--- src/main/java/leetCodeProblems/LeetCodeProblem1365.java
+++ src/main/java/leetCodeProblems/LeetCodeProblem1365.java
@@ -4,7 +4,36 @@ public class LeetCodeProblem1365 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		LeetCodeProblem1365.Solution obj = new LeetCodeProblem1365().new Solution();
+		
+		int [] nums = {8,1,2,2,3};
+		int[] numsoutput = obj.smallerNumbersThanCurrent(nums);
+		for(int i=0; i<numsoutput.length;i++)
+		System.out.println(numsoutput[i]);
 
+	}
+	
+	 class Solution {
+	    public  int[] smallerNumbersThanCurrent(int[] nums) {
+	        int count[] = new int [nums.length];
+	        int counter ;
+	        for(int i=0;i<nums.length;i++){
+	        	counter =0;
+	            for(int j=0;j<nums.length;j++){
+                    System.out.println("i : "+ i + " i value:" + nums[i]);
+                    System.out.println("j value : "+ j + " j value:" + nums[j]);
+	                if(nums[i]>nums[j]){
+	                    counter++;
+	                    System.out.println("counter:"+counter);   
+	                    count[i] = counter; 
+	                }
+	                
+                    System.out.println("counter array value at "+ i +":"+count[i]);
+	            }
+	        }
+	        return count;
+	    }
 	}
 
 }
